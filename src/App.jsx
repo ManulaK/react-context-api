41a5875c2d6fb243fb1c05c4fa0ef1083fbf5b44
ManulaK <it21019968@my.sliit.lk>
2023-05-13 09:57:30 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddUser from "./Pages/User/AddUser";
 import Home from "./Pages/Home/Home";
 import Equipment from "./Pages/Equipment/Equipment";
+import { EquipmentProvider } from "./provider/EquipmentProvider";
 export default function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/user' element={<UserPage />} />
-          <Route path='/addUser' element={<AddUser />} />
-          <Route path='/equipment' element={<Equipment />} />
-          <Route path='*' element={<h1>Not Found</h1>} />
-        </Routes>
-      </BrowserRouter>
+      <EquipmentProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/user' element={<UserPage />} />
+            <Route path='/addUser' element={<AddUser />} />
+            <Route path='/equipment' element={<Equipment />} />
+            <Route path='*' element={<h1>Not Found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </EquipmentProvider>
     </UserProvider>
   );
 }
